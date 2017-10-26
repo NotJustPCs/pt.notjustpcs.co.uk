43f4c3418e03d98829e7d50441f84e25cd160abb
Peter Fossey <peter@fossweb.net>
2017-10-26 13:40:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function pullQueryString() {
 }
 
 function WriteOneLink(URLprefix,URLsuffix,TelNo,FAicon,LinkDesc) {
-	return "<a class='list-group-item' target='_blank' href='" + URLprefix + TelNo + URLsuffix + "><i class='fa fa-" + FAicon + " fa-fw' aria-hidden='true'></i>&nbsp; " + LinkDesc + "</a>"
+	return "<a class='list-group-item' target='_blank' href='" + URLprefix + TelNo + URLsuffix + "><i class='fa fa-" + FAicon + " fa-fw' aria-hidden='true'></i>&nbsp; " + LinkDesc + "</a>";
 }
 
 function WriteLinks(IDtoWrite) {
-	WLlinks = WriteOneLink("https://www.facebook.com/search/top/?q=","",document.getElementById('TelNo').value,"facebook","Facebook")
+	WLlinks = WriteOneLink("https://www.facebook.com/search/top/?q=","",document.getElementById('TelNo').value,"facebook","Facebook");
 	WLlinks = WLlinks + "</br>" + WriteOneLink("https://www.google.co.uk/#q=","",document.getElementById('TelNo').value,"google","Google")
 	WLlinks = WLlinks + "</br>" + WriteOneLink("https://contacts.google.com/u/0/preview/search/","",document.getElementById('TelNo').value,"address-book-o","Google Contacts")
 	WLlinks = WLlinks + "</br>" + WriteOneLink("https://portal.aql.com/telecoms/network_lookup.php?number=","&nlSubmit=submit",document.getElementById('TelNo').value,"sitemap","Current supplier")
