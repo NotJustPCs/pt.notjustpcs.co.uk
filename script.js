8ef1d8f5e4459c0d0ef38362c738538c8adc0d46
Peter Fossey <peter@fossweb.net>
2017-10-28 09:26:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function WriteLinks(IDtoWrite) {
 	WLlinks = WLlinks + WriteOneLink("https://www.google.co.uk/#q=","",document.getElementById('PhoneNum').value,"google","Google")
 	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/u/0/preview/search/","",document.getElementById('PhoneNum').value,"address-book-o","Google Contacts")
 	WLlinks = WLlinks + WriteOneLink("https://portal.aql.com/telecoms/network_lookup.php?number=","&nlSubmit=submit",document.getElementById('PhoneNum').value,"sitemap","Current supplier")
-	WLlinks = WLlinks + WriteOneLink("https://www.ukphoneinfo.com/area-code/","",document.getElementById('PhoneNum').value.substring(0, 5),"sitemap","Area code lookup")
-	WLlinks = WLlinks + WriteOneLink("tel:","",document.getElementById('PhoneNum').value,"sitemap","Call number")
-	WLlinks = WLlinks + WriteOneLink("https://pt.notjustpcs.co.uk/?","",document.getElementById('PhoneNum').value,"sitemap","Shortcut this page")
+	WLlinks = WLlinks + WriteOneLink("https://www.ukphoneinfo.com/area-code/","",document.getElementById('PhoneNum').value.substring(0, 5),"map-pin","Area code lookup")
+	WLlinks = WLlinks + WriteOneLink("tel:","",document.getElementById('PhoneNum').value,"phone","Call number")
+	WLlinks = WLlinks + WriteOneLink("https://pt.notjustpcs.co.uk/?","",document.getElementById('PhoneNum').value,"link","Shortcut this page")
 	document.getElementById(IDtoWrite).innerHTML = WLlinks;
 }
