a1e360f00281b35002e6d732218e6626e65ebfb3
Peter Fossey <peter@fossweb.net>
2017-11-08 09:23:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function WriteOneLink(URLprefix,URLsuffix,TelNo,FAicon,LinkDesc) {
 function WriteLinks(IDtoWrite) {
 	WLlinks = WriteOneLink("https://www.facebook.com/search/top/?q=","",document.getElementById('PhoneNum').value,"facebook","Facebook");
 	WLlinks = WLlinks + WriteOneLink("https://www.google.co.uk/#q=","",document.getElementById('PhoneNum').value,"google","Google")
-	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/u/0/preview/search/","",document.getElementById('PhoneNum').value,"address-book-o","Google Contacts")
+	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/search/","",document.getElementById('PhoneNum').value,"address-book-o","Google Contacts")
 	WLlinks = WLlinks + WriteOneLink("https://portal.aql.com/telecoms/network_lookup.php?number=","&nlSubmit=submit",document.getElementById('PhoneNum').value,"sitemap","Current supplier")
 	WLlinks = WLlinks + WriteOneLink("https://www.ukphoneinfo.com/area-code/","",document.getElementById('PhoneNum').value.substring(0, 5),"map-pin","Area code lookup")
 	WLlinks = WLlinks + WriteOneLink("tel:","",document.getElementById('PhoneNum').value,"phone","Call number")
