63cda5cbedb575eb0fd47b6468a42573f106e8b3
Peter Fossey <peter@fossweb.net>
2017-11-08 09:35:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function WriteLinks(IDtoWrite) {
 	var PhoneNum = document.getElementById('PhoneNum').value;
 	WLlinks = WriteOneLink("https://www.facebook.com/search/top/?q=","",PhoneNum,"facebook","Facebook");
 	WLlinks = WLlinks + WriteOneLink("https://www.google.co.uk/#q=","",PhoneNum,"google","Google")
-	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/search/","",PhoneNum.substring(PhoneNum.length, -10),"address-book-o","Google Contacts")
+	WLlinks = WLlinks + WriteOneLink("https://contacts.google.com/search/","",PhoneNum.substring(PhoneNum.length - 10),"address-book-o","Google Contacts")
 	WLlinks = WLlinks + WriteOneLink("https://portal.aql.com/telecoms/network_lookup.php?number=","&nlSubmit=submit",PhoneNum,"sitemap","Current supplier")
 	WLlinks = WLlinks + WriteOneLink("https://www.ukphoneinfo.com/area-code/","",PhoneNum.substring(0, 5),"map-pin","Area code lookup")
 	WLlinks = WLlinks + WriteOneLink("tel:","",PhoneNum,"phone","Call number")
